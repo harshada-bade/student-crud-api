39d7b8250f512f46e1cde939653706660af92466
one2n <one2n@one2ns-MacBook-Air.local>
2026-06-10 16:17:25 +0530

    feat: add Student model

---
+++ app/models/student.py
@@ -0,0 +1,19 @@
+from app import db
+
+class Student(db.Model):
+    __tablename__ = "students"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    grade = db.Column(db.String(2), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "age": self.age,
+            "grade": self.grade
+        }
